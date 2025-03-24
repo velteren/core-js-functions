3aad525715807b23e6c9417a1995cf872d5d57e5
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-03-24 20:31:24 +0300

    feat: add getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -216,8 +216,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentNumber = startFrom - 1;
+  return function generator() {
+    currentNumber += 1;
+    return currentNumber;
+  };
 }
 
 module.exports = {
