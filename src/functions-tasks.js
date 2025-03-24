ee93f270db946c7497932a82f97cee33edf5de66
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-03-24 19:45:22 +0300

    feat: add getPolynom

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -90,8 +90,16 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (!args) return null;
+  return function polynom(x) {
+    let result = 0;
+    for (let i = 0; i < args.length; i += 1) {
+      const pow = args.length - 1 - i;
+      result += args[i] * x ** pow;
+    }
+    return result;
+  };
 }
 
 /**
