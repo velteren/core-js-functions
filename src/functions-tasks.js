9cc9fe06fbf37294f98a052a84a6d3610a36ae66
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-03-24 21:34:23 +0300

    feat: add partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -195,8 +195,11 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function func(...args2) {
+    const args = [...args1, ...args2];
+    return fn.apply(this, args);
+  };
 }
 
 /**
